6c73014eee2abc5843d793fff2e6531394f48926
AlinkaKuznets <linaolen6@mail.ru>
2024-11-25 00:31:10 +0700

    Added instructions, refactoring

--- src/components/Anketa/Anketa.tsx
+++ src/components/Anketa/Anketa.tsx
@@ -1,102 +1,114 @@
-"use client"
-import React, { FormEvent, useState, useEffect } from 'react'
+'use client';
+import React, { FormEvent, useState, useEffect } from 'react';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import NameField from './NameField';
 import EmailField from './EmailField';
 import PhoneField from './PhoneField';
 import MessageField from './MessageField';
-import { sendFeedback } from '../../app/data/sendFeedback'
-
-
+import { sendFeedback } from '../../app/data/sendFeedback';
 
 export default function Anketa() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
-    const [message, setMessage] = useState('');
-    const [consent, setConsent] = useState(false);
-    const [error, setError] = useState('');
-    const [labelText, setLabelText] = useState('Согласие на обработку персональных данных');
+   const [name, setName] = useState('');
+   const [email, setEmail] = useState('');
+   const [phone, setPhone] = useState('');
+   const [message, setMessage] = useState('');
+   const [consent, setConsent] = useState(false);
+   const [error, setError] = useState('');
+   const [labelText, setLabelText] = useState(
+      'Согласие на обработку персональных данных',
+   );
 
-    const updateLabelText = () => {
-        if (window.innerWidth < 600) {
-            setLabelText('Нажимая “Отправить”, Вы даете согласие на обработку персональных данных');
-        } else {
-            setLabelText('Согласие на обработку персональных данных');
-        }
-    };
+   const updateLabelText = () => {
+      if (window.innerWidth < 600) {
+         setLabelText(
+            'Нажимая “Отправить”, Вы даете согласие на обработку персональных данных',
+         );
+      } else {
+         setLabelText('Согласие на обработку персональных данных');
+      }
+   };
 
-    useEffect(() => {
-        updateLabelText();
-        window.addEventListener('resize', updateLabelText);
+   useEffect(() => {
+      updateLabelText();
+      window.addEventListener('resize', updateLabelText);
 
-        return () => {
-            window.removeEventListener('resize', updateLabelText);
-        };
-    }, []);
+      return () => {
+         window.removeEventListener('resize', updateLabelText);
+      };
+   }, []);
 
+   const handleSubmit = async (e: FormEvent) => {
+      e.preventDefault();
 
-    const handleSubmit = async (e: FormEvent) => {
-        e.preventDefault();
+      if (!consent) {
+         setError('Необходимо согласие на обработку персональных данных');
+         return;
+      }
 
-        if (!consent) {
-            setError('Необходимо согласие на обработку персональных данных');
-            return;
-        }
+      const feedback: Feedback = {
+         phone: phone,
+         email: email,
+         message: message,
+      };
 
-        const feedback: Feedback = {
-            phone: phone,
-            email: email,
-            message: message,
-        }
-
-        try {
-            const response = await sendFeedback(feedback);
-            if (response.ok) {
-                alert('Форма успешно отправлена');
-                setName('');
-                setEmail('');
-                setPhone('');
-                setMessage('');
-                setConsent(false);
-                setError('');
-            } else {
-                console.error('Ошибка при отправке формы');
-                setError('Произошла ошибка при отправке формы');
-            }
-        } catch (error) {
-            console.error('Ошибка:', error);
+      try {
+         const response = await sendFeedback(feedback);
+         if (response.ok) {
+            alert('Форма успешно отправлена');
+            setName('');
+            setEmail('');
+            setPhone('');
+            setMessage('');
+            setConsent(false);
+            setError('');
+         } else {
+            console.error('Ошибка при отправке формы');
             setError('Произошла ошибка при отправке формы');
-        }
-    };
+         }
+      } catch (error) {
+         console.error('Ошибка:', error);
+         setError('Произошла ошибка при отправке формы');
+      }
+   };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <div className='flex gap-10 mt-10 flex-col sm:flex-row'>
-                <NameField value={name} onChange={setName} />
-                <EmailField value={email} onChange={setEmail} />
-                <PhoneField value={phone} onChange={setPhone} />
-            </div>
-            <MessageField value={message} onChange={setMessage} />
-            <div className='checkbox-data'>
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            sx={{ color: 'white', '&.Mui-checked': { color: 'white' } }}
-                            checked={consent}
-                            onChange={(e) => setConsent(e.target.checked)}
-                            className='checkbox'
-                        />
-                    }
-                    label={<span className='personal-data lg:text-l text-sm'>{labelText}</span>}
-                />
-            </div>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            <div className='flex place-content-center my-10'>
-                <button type='submit' className='submit-button rounded-3xl bg-[#2D76F9] px-10 py-3'>Обсудить проект</button>
-            </div>
-        </form>
-    );
+   return (
+      <form onSubmit={handleSubmit}>
+         <div className="flex gap-10 mt-10 flex-col sm:flex-row">
+            <NameField value={name} onChange={setName} />
+            <EmailField value={email} onChange={setEmail} />
+            <PhoneField value={phone} onChange={setPhone} />
+         </div>
+         <MessageField value={message} onChange={setMessage} />
+         <div className="checkbox-data">
+            <FormControlLabel
+               control={
+                  <Checkbox
+                     sx={{
+                        color: 'white',
+                        '&.Mui-checked': { color: 'white' },
+                     }}
+                     checked={consent}
+                     onChange={(e) => setConsent(e.target.checked)}
+                     className="checkbox"
+                  />
+               }
+               label={
+                  <span className="personal-data lg:text-l text-sm">
+                     {labelText}
+                  </span>
+               }
+            />
+         </div>
+         {error && <p style={{ color: 'red' }}>{error}</p>}
+         <div>
+            <button
+               type="submit"
+               className="submit-button rounded-3xl"
+            >
+               Обсудить проект
+            </button>
+         </div>
+      </form>
+   );
 }
-
